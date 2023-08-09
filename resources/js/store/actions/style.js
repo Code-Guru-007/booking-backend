1e60382b197226d592c9cd5f7aa186db5079387a
Nemesis <127772153+Code-Guru-007@users.noreply.github.com>
2023-08-09 16:47:26 +0900

    remove

--- resources/js/store/actions/style.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const styleListAction = () => dispatch => {
-    axios.post('/styles')
-        .then(res => dispatch({type:'SET_STYLES', payload:res.data.styles}));
-};
